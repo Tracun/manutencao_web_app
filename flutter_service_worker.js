e78fe5cd68d844209a37b003ee8db890171773a2
Tracun <lucas060994@gmail.com>
2020-09-16 18:22:46 -0300

    quinto teste

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -22,7 +22,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "722e753df2d8e4f992fc103f9cbbb16c",
 "/": "722e753df2d8e4f992fc103f9cbbb16c",
-"main.dart.js": "ccf602b623a4c15daf38b8e4330137be",
+"main.dart.js": "a967053d799acff184bfbd0ba486b244",
 "manifest.json": "661df3835170adb94f5691e47f8747f2"
 };
 
